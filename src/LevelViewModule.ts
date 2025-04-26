ce44e569bbcf7ef018e9744ab53c7b78faf6c884
bakabird <bakabird@sina.cn>
2025-04-26 22:32:35 +0800

    Update LevelViewModule.ts

--- src/LevelViewModule.ts
+++ src/LevelViewModule.ts
@@ -9,6 +9,8 @@ export default class LevelViewModule implements ModuleInterface {
 
     private stable: boolean = false
     private isOpen: boolean = false;
+    // For Render
+    private root: any;
 
     init() {
         // 模块初始化逻辑
@@ -20,6 +22,8 @@ export default class LevelViewModule implements ModuleInterface {
                 this.isOpen = true
                 this.stable = true
             }
+            var root = data.Get("Root");
+            this.root = root
         }
     }
 
@@ -29,6 +33,26 @@ export default class LevelViewModule implements ModuleInterface {
             if (ImGui.Button("Stop LevelView")) {
                 wsMgr.send(WebDataFactory.OpenFunc("LevelView", false))
                 this.isOpen = false
+            }
+            if (this.root) {
+                // {"name":"Lobby","kits":[],"kitNum":0,"childs":[{"name":"0","kits":[],"kitNum":1}]}
+                // Render as tree
+                if (ImGui.TreeNode(this.root.name)) {
+                    for (var i = 0; i < this.root.childs.length; i++) {
+                        var child = this.root.childs[i];
+                        // As Node
+                        if (ImGui.TreeNode(child.name)) {
+                            // Render kits
+                            for (var j = 0; j < child.kits.length; j++) {
+                                var kit = child.kits[j];
+                                if (ImGui.Selectable(kit.name)) {
+                                }
+                            }
+                            ImGui.TreePop();
+                        }
+                    }
+                    ImGui.TreePop();
+                }
             }
         }
         else {
